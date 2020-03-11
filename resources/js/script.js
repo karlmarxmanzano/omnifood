c7f1501d05557aef306c2d3f3a1bde00a7be6f11
Karl Marx Manzano <karlmarxmanzano@gmail.com>
2020-03-11 21:21:27 +0800

    Navbar mobile responsiveness added

--- resources/js/script.js
+++ resources/js/script.js
@@ -69,6 +69,39 @@ $(document).ready(function () {
     }, {
         offset: '50%'
     })
+
+    $('.js-wp-2').waypoint(function (direction) {
+        $('.js-wp-2').addClass('animated fadeInUp')
+    }, {
+        offset: '50%'
+    })
+
+    $('.js-wp-3').waypoint(function (direction) {
+        $('.js-wp-3').addClass('animated fadeIn')
+    }, {
+        offset: '50%'
+    })
+
+    $('.js-wp-4').waypoint(function (direction) {
+        $('.js-wp-4').addClass('animated pulse')
+    }, {
+        offset: '50%'
+    })
+
+    $('.js-nav-icon').click(function () {
+        let nav = $('.js-main-nav')
+        let icon = $('.js-nav-icon i')
+
+        nav.slideToggle(200)
+
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }        
+    })
     
 
 })
